964e4053a5c62b18e0dbec41293955f46ef00897
rdh.rhmtllah <rdh.rhmtllah@gmail.com>
2025-07-08 19:00:50 +0700

    push to gcloud

--- routes/web.php
+++ routes/web.php
@@ -37,14 +37,11 @@ Route::post('/post-password', function(Request $request){
 
 Route::get('/test-db', function () {
 try {
-    // Coba paksa koneksi ke database default
     DB::connection()->getPdo();
 
-    // Jika baris di atas tidak error, koneksi berhasil
     return '<h1 style="color:green;">✅ SUKSES! Koneksi ke database berjalan dengan baik.</h1>';
 
 } catch (\Exception $e) {
-    // Jika ada error saat koneksi, tampilkan pesannya
     return '<h1 style="color:red;">❌ GAGAL! Tidak bisa terhubung ke database.</h1><p>Error: ' . $e->getMessage() . '</p>';
 }
 });
